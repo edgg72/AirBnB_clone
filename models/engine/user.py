ce00c9b70fa608ee163999aa555d8969a30e0d83
edgg72 <edgg72@hotmail.com>
2020-11-04 21:20:43 +0000

    adding classes

---
+++ models/engine/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" User class """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class which inherits from BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
